7f75b74052f2730d2aa29c15ef24c87e6db97378
灰级计萌 <78867289+YISHENGPINGAN51@users.noreply.github.com>
2022-01-16 22:34:40 +0800

    这是基于实验室王老师视频的KEIL4测试

---
+++ 蜂鸣器的应用（切歌暂停功能）/Source/delay.c
@@ -0,0 +1,14 @@
+#include "delay.h"
+
+/****************************************
+函数名：DelayXms
+功  能；毫秒级延时函数
+参  数；unsigned int
+返回值；无
+****************************************/
+void DelayXms(unsigned int xms)
+{
+  unsigned int i, j;  //0 ~ 65535  000H ~ FFFH
+  for (i = xms; i > 0; i--)
+    for (j = 124; j > 0; j--);
+}
